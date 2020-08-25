f0ebccc86d8f68950b8be5297eca5407c946569b
Kimberly Yu <kimberly331yu@gmail.com>
2020-08-24 18:00:12 -0700

    portfolio

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 function moveRight() {
-    var elem = document.getElementById("squirrel");
-    var elem2= document.getElementById("acorn");
-    var pos = 10;
-    var pos2 = -15;
-    var id = setInterval(frame, 1);
-    function frame() {
-      if (pos == 900) {
-        clearInterval(id);
-      } else {
-        pos+=2;
-        pos2+=2; 
-        elem2.style.left = pos2 + "px"; 
-        elem.style.left = pos + "px"; 
-      }
-      //for(var i = pos; i<900; i+=5){
-      //  elem.style.left = i + "px";
-      //}
-      //for(var i = pos2; i<950; i+=5){
-      //  elem2.style.left = i + "px";
-      //}
+  var elem = document.getElementById("squirrel");
+  var elem2= document.getElementById("acorn");
+  var pos = 10;
+  var pos2 = -15;
+  var id = setInterval(frame, 1);
+  function frame() {
+    if (pos == 900) {
+      clearInterval(id);
+    } else {
+      pos+=2;
+      pos2+=2; 
+      elem2.style.left = pos2 + "px"; 
+      elem.style.left = pos + "px"; 
     }
+    //for(var i = pos; i<900; i+=5){
+    //  elem.style.left = i + "px";
+    //}
+    //for(var i = pos2; i<950; i+=5){
+    //  elem2.style.left = i + "px";
+    //}
   }
+}
+
